b426042403bc19ca347801ebcee2c3554c2936fc
bright <yml_bright@163.com>
2015-03-16 10:21:40 +0800

    none

--- stegodetector.py
+++ stegodetector.py
@@ -74,9 +74,7 @@ class StegoDetector():
 
         fastscan(self.fileOject, self.fileType)
 
-        asc_detect(self.filePath, self.asciiDetectLength)
-
-        return
+        #asc_detect(self.filePath, self.asciiDetectLength)
 
         if not self.fastMod:
             imgDetector = self.mimeMap[self.fileType](self.fileOject)
@@ -97,5 +95,5 @@ class StegoDetector():
             # x.save('save.bmp', 'BMP')
             # do some check on rowdata
 
-t = StegoDetector(filePath='E:\\Github\\stegodetector\\decodetest\\test\\!index.jpg', fileType='jpg')
+t = StegoDetector(filePath='E:\\Github\\stegodetector\\decodetest\\test\\!test4127.png', fileType='png')
 t.start()
